e0ff5fe8d18c152a6a28c8e108e1c75586336372
ArtBekk <arturbekk@gmail.com>
2021-03-07 17:55:38 +0700

    Initial commit

---
+++ commands/gif.js
@@ -0,0 +1,29 @@
+client.on('message', msg => {
+    if (msg.content === '!pug') {
+        msg.channel.send('https://media.giphy.com/media/ZX63w9xm4ilgs/giphy.gif');
+    }
+});
+
+client.on('message', msg => {
+    if (msg.content === '!hehe') {
+        msg.channel.send('https://media.giphy.com/media/dPEJxh06y4OTC/giphy.gif');
+    }
+});
+
+client.on('message', msg => {
+    if (msg.content === '!noice') {
+        msg.channel.send('https://media.giphy.com/media/yJFeycRK2DB4c/giphy.gif');
+    }
+});
+
+client.on('message', msg => {
+    if (msg.content === '!пиздец') {
+        msg.channel.send('https://media.giphy.com/media/Zqe1S3qNQxsuQ/giphy.gif');
+    }
+});
+
+client.on('message', msg => {
+    if (msg.content === '!wiggle') {
+        msg.channel.send('https://media.giphy.com/media/b3Gp6a25caNZC/giphy.gif');
+    }
+});
---
+++ handler.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+
+const gif = require ("./commands/gif.js")
+const ytPlayer = require("./commands/ytPlayer.js")
+
+module.exports = async function (msg) {
+    if (msg.guild.id === '336199770185924608')
+    {
+        if(msg.content[0] === '!')
+        {
+
+        }
+    }
+}
