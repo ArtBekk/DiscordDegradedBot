3d324f2d36d57bb60eaf030bdb88de0030ccefc3
ArtBekk <arturbekk@gmail.com>
2021-03-08 14:30:29 +0700

    Refactored with switch

--- commands/gif.js
+++ commands/gif.js
@@ -1,29 +1,22 @@
-client.on('message', msg => {
-    if (msg.content === '!pug') {
-        msg.channel.send('https://media.giphy.com/media/ZX63w9xm4ilgs/giphy.gif');
+module.exports = function (msg) {
+    console.log("Sending gif");
+    switch (msg.content) {
+        case '!pug':
+            msg.channel.send('https://media.giphy.com/media/ZX63w9xm4ilgs/giphy.gif');
+            break;
+        case '!hehe':
+            msg.channel.send('https://media.giphy.com/media/dPEJxh06y4OTC/giphy.gif');
+            break;
+        case '!noice':
+            msg.channel.send('https://media.giphy.com/media/yJFeycRK2DB4c/giphy.gif');
+            break;
+        case '!пиздец':
+            msg.channel.send('https://media.giphy.com/media/Zqe1S3qNQxsuQ/giphy.gif');
+            break;
+        case '!wiggle':
+            msg.channel.send('https://media.giphy.com/media/b3Gp6a25caNZC/giphy.gif');
+            break;
+        default:
+            console.log('No such gif');
     }
-});
-
-client.on('message', msg => {
-    if (msg.content === '!hehe') {
-        msg.channel.send('https://media.giphy.com/media/dPEJxh06y4OTC/giphy.gif');
-    }
-});
-
-client.on('message', msg => {
-    if (msg.content === '!noice') {
-        msg.channel.send('https://media.giphy.com/media/yJFeycRK2DB4c/giphy.gif');
-    }
-});
-
-client.on('message', msg => {
-    if (msg.content === '!пиздец') {
-        msg.channel.send('https://media.giphy.com/media/Zqe1S3qNQxsuQ/giphy.gif');
-    }
-});
-
-client.on('message', msg => {
-    if (msg.content === '!wiggle') {
-        msg.channel.send('https://media.giphy.com/media/b3Gp6a25caNZC/giphy.gif');
-    }
-});
+}
