17cfb1c3c4d65c4c70fc8641551a6d112c43922c
ArtBekk <arturbekk@gmail.com>
2021-03-08 15:10:10 +0700

    Added support for searching GIFs from Tenor

--- commands/gif.js
+++ commands/gif.js
@@ -1,23 +1,34 @@
-module.exports = function (msg) {
+const fetch = require("node-fetch");
+
+module.exports = function (msg, args) {
     console.log("Sending gif");
-    switch (msg.content) {
-        case '!pug':
+    switch (args) {
+        case 'pug':
             msg.channel.send('https://media.giphy.com/media/ZX63w9xm4ilgs/giphy.gif');
             break;
-        case '!hehe':
+        case 'hehe':
             msg.channel.send('https://media.giphy.com/media/dPEJxh06y4OTC/giphy.gif');
             break;
-        case '!noice':
+        case 'noice':
             msg.channel.send('https://media.giphy.com/media/yJFeycRK2DB4c/giphy.gif');
             break;
-        case '!пиздец':
+        case 'пиздец':
             msg.channel.send('https://media.giphy.com/media/Zqe1S3qNQxsuQ/giphy.gif');
             break;
-        case '!wiggle':
+        case 'wiggle':
             msg.channel.send('https://media.giphy.com/media/b3Gp6a25caNZC/giphy.gif');
             break;
         default:
-            console.log('No such gif');
+            console.log('Making a request for random gif from Tenor')
+            let keywords;
+            if (args.length > 0) {
+                keywords = args.join(" ");
+            }
+            let url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENORKEY}`;
+            let response = await fetch(url);
+            let json = await response.json();
+            const index = Math.floor(Math.random() * json.results.length);
+            msg.channel.send(json.results[index].url);
             break;
     }
 }
