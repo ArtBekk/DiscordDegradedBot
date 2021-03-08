9c5e7d88a72427f22f3a0b3222508963ad7f29c8
ArtBekk <arturbekk@gmail.com>
2021-03-08 14:29:58 +0700

    Introduced simple switch for selecting the right command

--- handler.js
+++ handler.js
@@ -1,14 +1,18 @@
-const Discord = require('discord.js');
-
 const gif = require ("./commands/gif.js")
 const ytPlayer = require("./commands/ytPlayer.js")
 
 module.exports = async function (msg) {
     if (msg.guild.id === '336199770185924608')
     {
-        if(msg.content[0] === '!')
+        switch (msg.content)
         {
-
+            case '!play':
+                console.log('Music request');
+                //TODO implement player from youtube
+                break;
+            default:
+                console.log('Gif request');
+                gif(msg);
         }
     }
 }
