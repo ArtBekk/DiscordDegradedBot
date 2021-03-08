a331d91a53b33c932ead43cc20bb0527ab5fc1ee
ArtBekk <arturbekk@gmail.com>
2021-03-08 15:29:49 +0700

    Fixed request for Tenor

--- commands/gif.js
+++ commands/gif.js
@@ -20,11 +20,7 @@ module.exports = function (msg, request) {
             break;
         default:
             console.log('Making a request for random gif from Tenor')
-            let keywords;
-            if (args.length > 0) {
-                keywords = args.join(" ");
-            }
-            let url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENORKEY}`;
+            let url = `https://api.tenor.com/v1/search?q=${request}&key=${process.env.TENORKEY}`;
             let response = fetch(url);
             let json = response.json();
             const index = Math.floor(Math.random() * json.results.length);
