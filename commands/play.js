5dbbbcb93a8f6bfc899fb2924b6a7c52d2a24c1d
ArtBekk <arturbekk@gmail.com>
2021-03-09 12:42:03 +0700

    Added support for yt links

--- commands/play.js
+++ commands/play.js
@@ -8,6 +8,31 @@ module.exports = async function (msg, args) {
 
     if (!args.length) msg.reply("Don't spam commands without actually telling me what to do idiot");
 
+    const validURL = (str) => {
+        var regex = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+        if (!regex.test(str)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    if (validURL(args[0])) {
+
+        const connection = await voiceChannel.join();
+        const stream = ytdl(args[0], {filter: 'audioonly'});
+
+        connection.play(stream, {seek: 0, volume: 1})
+            .on('finish', () => {
+                voiceChannel.leave();
+                msg.channel.send('Your taste sucks, never call me again');
+            });
+
+        await message.reply(`Playing from link ${args} or whatever`)
+
+        return
+    }
+
     const connection = await voiceChannel.join();
     const videoFinder = async (query) => {
         const videoResult = await ytSearch(query);
@@ -21,6 +46,8 @@ module.exports = async function (msg, args) {
         connection.play(stream, {seek: 0, volume: 1})
             .on('finish', () => {
                 voiceChannel.leave();
+                msg.channel.send('Your taste sucks, never call me again');
             });
+        await msg.reply(`Playing some "${args}" or whatever`);
     }
 }
