d89354cf7df4fdb19d321d2512ca53bd3b7bf163
ArtBekk <arturbekk@gmail.com>
2021-05-04 12:26:57 +0700

    Added check for permissions in purge command

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import threading
 
 import discord
+import discord.ext
 import discord_slash
 from discord_slash.utils.manage_commands import create_option
 from dotenv import load_dotenv
@@ -27,20 +28,22 @@ async def status(ctx):
     await ctx.send(content="I'm more alive than you, fag")
 
 
+# add some kind of exception handling later
 @slashCommandHandler.slash(name="purge",
                            description="When you don't want any witnesses",
                            options=[
                                create_option(
                                    name="messageNum",
-                                   description="How much garbage shall I remove?",
+                                   description="How much garbage should I remove?",
                                    option_type=4,
                                    required=True
                                )
                            ]
                            )
+@discord.ext.commands.has_permissions(manage_messages=True)
 async def purge(ctx, messageNum: int):
     await client.get_channel(ctx.channel_id).purge(limit=messageNum)
-    await ctx.send(content="Succass")
+    await ctx.send(content="They say history is written by the winners but you're clearly a loser, whatevs")
 
 
 @slashCommandHandler.slash(name="stonks_check",
@@ -48,7 +51,7 @@ async def purge(ctx, messageNum: int):
                            options=[
                                create_option(
                                    name="market_place",
-                                   description=r"Select shop you want to get your prices from where",
+                                   description=r"Select shop you want to get your prices from",
                                    option_type=3,
                                    required=True
                                )
