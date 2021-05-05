9d674c00549b53463e86c91a585c057b721edc27
ArtBekk <arturbekk@gmail.com>
2021-05-05 16:54:05 +0700

    Fixed messages and added corresponding pictures

--- priceTracker.py
+++ priceTracker.py
@@ -18,11 +18,14 @@ async def check_market(channel):
             message = f"Current best price {metro.get('price')} деревянных in {metro.get('marketName')}, da link {metro.get('URL')}"
         if price != current_price and price is not None:
             if price > current_price:
-                await channel.send("THE END IS NEAR, PRICE IS RISING, STOCKPILE WHILE YOU CAN")
-            elif price < current_price:
+                await channel.send("https://imgur.com/m4Vertv")
                 await channel.send("Yet another beautiful day for fans of doshirak: price went down")
+            elif price < current_price:
+                await channel.send("https://imgur.com/RzJItGX")
+                await channel.send("THE END IS NEAR, PRICE IS RISING, STOCKPILE WHILE YOU CAN")
             elif price == current_price:
+                await channel.send("https://i.imgur.com/DeyLAlZh.jpg")
                 await channel.send("No price fluctuation and no idea what it means either")
         price = current_price
         await channel.send(message)
-        await asyncio.sleep(86400)
+        await asyncio.sleep(21600)
